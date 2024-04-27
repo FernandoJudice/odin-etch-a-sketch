1869e7d2c6b1f9ddfe0aae8c1139f797704bdff4
Fernando Judice <fernandocesar95@gmail.com>
2024-04-26 23:07:34 -0300

    populate div in a grid like pattern

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ let gridSize = 16;
 
 const drawArea = document.querySelector(".drawArea");
 
-for (let i=0;i<gridSize;i++) {
+for (let i=0;i<gridSize*gridSize;i++) {
     const square = document.createElement("div");
     square.classList.add("square");
-    square.textContent = "a"
+    square.textContent = i;
+    square.style.flex = `1 0 calc(1/${gridSize+1}*100%)`;
+    square.style.border = "solid gray 1px"
     drawArea.appendChild(square);
 }
 
