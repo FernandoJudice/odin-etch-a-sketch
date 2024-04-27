33c66c3132a001c5098bed589cba02849cca02c0
Fernando Judice <fernandocesar95@gmail.com>
2024-04-27 10:08:51 -0300

    add clear grid function

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-let gridSize = 16;
-
 const drawArea = document.querySelector(".drawArea");
+const button  =document.querySelector("button");
 
+renderGrid(16);
 
-for (let i=0;i<gridSize;i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    drawArea.appendChild(row);
-    
-    for (let j=0;j<gridSize;j++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        square.style.flex = "1 0 auto";
-        square.style.border = "solid gray 1px"
-        square.onmouseenter = () => square.style.background = "black";
-        row.appendChild(square);
+button.onclick = clearGrid;
+
+function renderGrid(gridSize){   
+    for (let i=0;i<gridSize;i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        drawArea.appendChild(row);
+        
+        for (let j=0;j<gridSize;j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.style.flex = "1 0 auto";
+            square.style.border = "solid gray 1px"
+            square.onmouseenter = () => square.style.background = "black";
+            row.appendChild(square);
+        }
+    }
+}
+
+function clearGrid(){
+    while(drawArea.firstChild) {
+        drawArea.removeChild(drawArea.firstChild);
     }
 }
